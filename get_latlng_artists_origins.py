d5ab27037581eac3843a9cd65c26fdd9b140e34d
mattiagatti <gatti.mattia00@gmail.com>
2022-11-15 23:58:46 +0100

    commit

--- get_latlng_artists_origins.py
+++ get_latlng_artists_origins.py
@@ -12,7 +12,7 @@ def get_latlng(origin_string):
             latitude, longitude = location.latitude, location.longitude
         except:
             pass
-    print(latitude, longitude)
+    # print(latitude, longitude)
     return latitude, longitude
 
 
@@ -24,11 +24,15 @@ def get_formatted_country(lat, lng):
 
 if __name__ == "__main__":
     billboard_artists_data = pd.read_csv("datasets/billboard_artists.csv")
+
     billboard_artists_data["origin"] = billboard_artists_data["origin"].apply(
         lambda x: x.replace("U.S.", "United States") if not pd.isna(x) else x)
+    billboard_artists_data["origin"] = billboard_artists_data["origin"].apply(
+        lambda x: x.replace("U.K.", "United Kingdom") if not pd.isna(x) else x)
 
     billboard_artists_data.insert(3, "lat", None)
     billboard_artists_data.insert(4, "lng", None)
+
     for index in range(len(billboard_artists_data)):
         origin_string = billboard_artists_data.iloc[index, 2]
         latitude, longitude = get_latlng(origin_string)
