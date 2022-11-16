c7077254689193333363fe50a11cfbb6ca5d1b49
mattiagatti <gatti.mattia00@gmail.com>
2022-11-16 13:52:33 +0100

    commit

--- get_formatted_country.py
+++ get_formatted_country.py
@@ -1,13 +1,19 @@
 from geopy import Nominatim
 import pandas as pd
 
-def get_formatted_country(lat, lng):
+def get_formatted_country(lat, lng, origin_string):
     try:
         locator = Nominatim(user_agent="myGeocoder")
         location = locator.reverse(f"{lat}, {lng}")
         address = location.raw["address"]
         print(address)
-        city = address["city"] if "city" in address else address["town"]
+        if "city" in address:
+            city = address["city"]
+        elif "town" in address:
+            city = address["town"]
+        else:
+            city = origin_string.split(",")[0]
+
         state, country = address["state"], address["country"]
     except:
         city, state, country = None, None, None
@@ -21,16 +27,17 @@ if __name__ == "__main__":
     for index in range(len(billboard_artists_data)):
         latitude = billboard_artists_data.iloc[index, 3]
         longitude = billboard_artists_data.iloc[index, 4]
-        city, country, state = get_formatted_country(latitude, longitude)
+        origin_string = billboard_artists_data.iloc[index, 2]
+        city, state, country = get_formatted_country(latitude, longitude, origin_string)
         cities.append(city)
         states.append(state)
         countries.append(country)
         billboard_artists_data.iloc[index, 3] = latitude
         billboard_artists_data.iloc[index, 4] = longitude
 
-    billboard_artists_data.drop("origin", axis=1)
-    billboard_artists_data.drop("latitude", axis=1)
-    billboard_artists_data.drop("longitude", axis=1)
+    billboard_artists_data = billboard_artists_data.drop("origin", axis=1)
+    billboard_artists_data = billboard_artists_data.drop("latitude", axis=1)
+    billboard_artists_data = billboard_artists_data.drop("longitude", axis=1)
 
     billboard_artists_data['city'] = cities
     billboard_artists_data['state'] = states
